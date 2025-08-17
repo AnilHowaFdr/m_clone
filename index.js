89bfc7aa8c4ffe559e2f1e1d281b613ba59eed89
Heron Mia <howa.anil@gmail.com>
2025-08-17 23:18:32 +0600

    nav

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-const slides = document.querySelectorAll(".slide");
-let count = 0;
+// const slides = document.querySelectorAll(".slide");
+// let count = 0;
 
-const changeSlide = () => {
-  slides[count].classList.remove("active");
-  count = (count + 1) % slides.length;
-  slides[count].classList.add("active");
-};
+// const changeSlide = () => {
+//   slides[count].classList.remove("active");
+//   count = (count + 1) % slides.length;
+//   slides[count].classList.add("active");
+// };
 
-setInterval(changeSlide, 4000);
+// setInterval(changeSlide, 4000);
 
-// ================= color picker start ==============
+// // ================= color picker start ==============
 
-document.querySelector(".pickerBtn").onclick = () => {
-  document.querySelector(".colorSwitcher").classList.toggle("active");
-};
+// document.querySelector(".pickerBtn").onclick = () => {
+//   document.querySelector(".colorSwitcher").classList.toggle("active");
+// };
 
-let themeBtns = document.querySelectorAll(".theme-btns");
+// let themeBtns = document.querySelectorAll(".theme-btns");
 
-themeBtns.forEach((color) => {
-  color.addEventListener("click", () => {
-    let dataColor = color.getAttribute("data-color");
-    document
-      .querySelector(":root")
-      .style.setProperty("--main-color", dataColor);
-  });
-});
+// themeBtns.forEach((color) => {
+//   color.addEventListener("click", () => {
+//     let dataColor = color.getAttribute("data-color");
+//     document
+//       .querySelector(":root")
+//       .style.setProperty("--main-color", dataColor);
+//   });
+// });
 
 // ================= color picker end ==============
+
+document.querySelector("#hello").innerHTML = "hello i am anil";
